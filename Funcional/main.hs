4f2a0df38c72de9480ce0a47e1ad3b467707ce5a
Nico Tomasino <nicolastomassino@gmail.com>
2026-01-31 02:20:58 -0300

    Incorporo base funcional desde rama de Alejo

--- Funcional/main.hs
+++ Funcional/main.hs
@@ -1,12 +1,1 @@
--- Archivo: funcional/Main.hs
--- Ejercicios / código del paradigma funcional (Haskell).
-
-main :: IO ()
-main = putStrLn "Hola, mundo funcional"
-
---comentarios
-
-{-
- Barra de comentarios
--}
 
